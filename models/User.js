4bfc000894cc6d45ebde947f5437378ad60a5a49
paperpatch <patchen21@gmail.com>
2021-12-25 14:20:01 -0500

    added email to user model

--- models/User.js
+++ models/User.js
@@ -26,6 +26,14 @@ User.init(
         notEmpty: true
       }
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
