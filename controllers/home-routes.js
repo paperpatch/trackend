f0cfec41b21af93db5bb118fe63da9631cccf7cf
paperpatch <patchen21@gmail.com>
2021-12-24 21:22:55 -0500

    added authentication to all get routes. all users need to log in. adjusted header for login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 // const sequelize = require('../config/connection');
 const { Post, User, Comment} = require('../models');
+const withAuth = require('../utils/auth');
 
 // get all posts for homepage
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
   console.log('======================');
   Post.findAll({
     attributes: [
@@ -43,7 +44,7 @@ router.get('/', (req, res) => {
 });
 
 // get single post
-router.get('/post/:id', (req, res) => {
+router.get('/post/:id', withAuth, (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id
