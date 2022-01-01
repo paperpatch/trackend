9b2e045bae81569f98ff98d8eb09625333db6c30
paperpatch <patchen21@gmail.com>
2022-01-01 16:04:58 -0500

    added more data to test out UI

--- seeds/ticket-seeds.js
+++ seeds/ticket-seeds.js
@@ -25,6 +25,38 @@ const ticketdata = [
     priority_id: 4,
     status_change_id: 1,
   },
+  {
+    title: 'Priority Tickets Work',
+    ticket_text: "Priority routes and models are not working correctly. Need immediate fix.",
+    status: true,
+    user_id: 3,
+    priority_id: 1,
+    status_change_id: 1,
+  },
+  {
+    title: 'Add Type Section',
+    ticket_text: "Just like with Priority and StatusChange. A type feature section would be nice. It would incorporate whether the issue is a bug, IT, feature or other issue type.",
+    status: true,
+    user_id: 1,
+    priority_id: 2,
+    status_change_id: 1,
+  },
+  {
+    title: 'Wireframe',
+    ticket_text: "For this project, we need a wireframe. We'll need one to start this project off.",
+    status: true,
+    user_id: 2,
+    priority_id: 1,
+    status_change_id: 2,
+  },
+  {
+    title: 'Minimal Viable Product',
+    ticket_text: "Need a functional project.",
+    status: true,
+    user_id: 1,
+    priority_id: 4,
+    status_change_id: 4,
+  },
 ];
 
 const seedTickets = () => Ticket.bulkCreate(ticketdata);
