6b70df930ab8422d96ed7f83bb98730a8b07b8bc
paperpatch <patchen21@gmail.com>
2022-01-02 22:53:42 -0500

    got rid of comments in priority route

--- controllers/priority-routes.js
+++ controllers/priority-routes.js
@@ -54,61 +54,6 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-// router.get('/:level', withAuth, (req, res) => {
-//   Priority.findAll({
-//     where: {
-//       level: req.params.level
-//     },
-//     attributes: [
-//       'id',
-//       'level',
-//     ],
-//     include: [
-//       {
-//         model: Ticket,
-//         attributes: ['id', 'ticket_text', 'title', 'status', 'priority_id', 'status_change_id', 'type_id', 'created_at'],
-//         include: [
-//           {
-//           model: Comment,
-//           attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
-//           },
-//           {
-//             model: User,
-//             attributes: ['username']
-//           },
-//           {
-//             model: StatusChange,
-//             attributes: ['statusChange']
-//           },
-//           {
-//             model: Comment,
-//             attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
-//             include: {
-//               model: User,
-//               attributes: ['username']
-//             }
-//           },
-//         ],
-//       },
-//     ],
-//   })
-//     .then(dbTicketData => {
-//       if (dbTicketData) {
-//         const ticket = dbTicketData.get({ plain: true });
-        
-//         res.render('priority', {
-//           ticket,
-//           loggedIn: true
-//         });
-//       } else {
-//         res.status(404).end();
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).json(err);
-//     });
-// });
-
 router.get('/:priority_id', withAuth, (req, res) => {
   console.log('======================');
   Ticket.findAll({
