158a0f13a46d10ff900def0b3f1dff88eb228ea3
paperpatch <patchen21@gmail.com>
2022-01-06 21:28:09 -0500

    changed Ticket route to User route

--- controllers/manage-routes.js
+++ controllers/manage-routes.js
@@ -7,66 +7,29 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, (req, res) => {
   console.log(req.session);
   console.log('======================');
-  Ticket.findAll({
-    // where: {
-    //   user_id: req.session.user_id
-    // },
+  User.findAll({
+    where: {
+      id: req.session.user_id
+    },
     attributes: [
       'id',
-      'ticket_text',
-      'title',
-      'status',
-      'priority_id',
-      'status_change_id',
-      'type_id',
-      'assigned_id',
-      'created_at',
+      'username',
+      'email',
+      'password',
+      'role_id',
     ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        as: 'user',
-        attributes: ['username', 'role_id',],
-        include: {
-          model: Role,
-          attributes: ['role']
-        }
-      },
-      {
-        model: User,
-        as: 'assign',
-        attributes: ['username'],
-      },
-      {
-        model: Priority,
-        attributes: ['level'],
-      },
-      {
-        model: StatusChange,
-        attributes: ['statusChange'],
-      },
-      {
-        model: Type,
-        attributes: ['type'],
-      },
-    ]
+    include: {
+      model: Role,
+      attributes: ['role']
+    }
   })
-    .then(dbTicketData => {
-      const tickets = dbTicketData.map(ticket => ticket.get({ plain: true }));
-      res.render('manage-users', { tickets, loggedIn: true });
+    .then(dbProfileData => {
+      const profile = dbProfileData.map(profile => profile.get({ plain: true }));
+      res.render('manage-users', { profile, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-
 module.exports = router;
