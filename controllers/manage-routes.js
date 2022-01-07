f491f5d6e83af75bd07fe73c480017e5b348e60b
paperpatch <patchen21@gmail.com>
2022-01-06 21:40:35 -0500

    fixed displaying user by eliminating condition. Deleted unused const models.

--- controllers/manage-routes.js
+++ controllers/manage-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Ticket, User, Comment, Priority, StatusChange, Type, Role} = require('../models');
+const { User, Role} = require('../models');
 const withAuth = require('../utils/auth');
 
 // get all Users for Managing Users
@@ -8,9 +8,6 @@ router.get('/', withAuth, (req, res) => {
   console.log(req.session);
   console.log('======================');
   User.findAll({
-    where: {
-      id: req.session.user_id
-    },
     attributes: [
       'id',
       'username',
