6f0b47ba99f9df99780d71213b11ff594746546b
paperpatch <patchen21@gmail.com>
2022-01-07 16:42:34 -0500

    changed tickets-routes to all-tickets-routes to prevent confusion. renamed handlebars and other files to reflect change

--- controllers/api/ticket-routes.js
+++ controllers/api/ticket-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Ticket, User, Comment, Priority, StatusChange, Type, Role} = require('../../models');
+const { Ticket, User, Comment, Priority, StatusChange, Type, Role } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // get all users
