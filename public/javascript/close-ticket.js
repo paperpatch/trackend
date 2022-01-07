80c012827939ac022eae43548472d6e63813c791
davezer <davezer27@gmail.com>
2022-01-07 17:07:20 -0500

    stashing again

--- public/javascript/close-ticket.js
+++ public/javascript/close-ticket.js
@@ -16,7 +16,7 @@ function closeTicket() {
 
     const title = document.querySelector('input[name="ticket-title"]').value.trim();
     const ticket_text = document.querySelector('textarea[name="ticket-text"]').value;
-    const status = JSON.parse(document.getElementById('status').value);
+    // const status = JSON.parse(document.getElementById('status').value);
   
     const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
