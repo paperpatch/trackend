82692686d863a16f195f91bc539b8d30b3114c5b
paperpatch <patchen21@gmail.com>
2022-01-06 23:13:05 -0500

    added guest login function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -22,4 +22,29 @@ async function loginFormHandler(event) {
   }
 }
 
+async function loginGuestHandler(event) {
+  event.preventDefault();
+
+  const username = 'guest';
+  const password = 'guest';
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('guest', loginGuestHandler);
