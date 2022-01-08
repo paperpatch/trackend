343e611c67c5fd98f65d98747b481943d9cbaf6e
davezer <davezer27@gmail.com>
2022-01-08 17:54:28 -0500

    wrote function for closing an open ticket

--- public/javascript/close-ticket.js
+++ public/javascript/close-ticket.js
@@ -1,14 +1,27 @@
-const title = document.querySelector('input[name="ticket-title"]').value.trim();
-const ticket_text = document.querySelector('textarea[name="ticket-text"]').value;
-const status = JSON.parse(document.getElementById('status').value);
-
 async function closeTicket() {
+    
+    let status = JSON.parse(document.getElementById('status').value);
 
     const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-    ];
-    
-    fetch(`/api/tickets/${id}`).then(function(response) {
-        console.log(response);
+            window.location.toString().split('/').length - 1
+            ];
+
+    status = !status 
+
+    const response = await fetch(`/api/tickets/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        
+        status
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-}
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
