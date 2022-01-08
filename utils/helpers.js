e33edc7a3a6ab456e7cb41c25719feb12ce9ca96
paperpatch <patchen21@gmail.com>
2022-01-07 21:27:20 -0500

    deleted more unused CSS. added upper case format helper and changed user to {{user_username}}

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,6 +4,7 @@ module.exports = {
       date
     ).getFullYear()}`;
   },
+
   format_url: url => {
     return url
       .replace('http://', '')
@@ -12,11 +13,16 @@ module.exports = {
       .split('/')[0]
       .split('?')[0];
   },
+
   format_plural: (word, amount) => {
     if (amount !== 1) {
       return `${word}s`;
     }
 
     return word;
-  }
+  },
+
+  format_upper: word => {
+    return word.toUpperCase();
+  },
 };
