bfec71d41df233657ffc1976c6ec38d069991e29
davezer <davezer27@gmail.com>
2022-01-08 19:49:04 -0500

    added function to check status of ticket upon reaching edit page

--- public/javascript/close-ticket.js
+++ public/javascript/close-ticket.js
@@ -1,13 +1,16 @@
+const title = document.querySelector('input[name="ticket-title"]').value.trim();
+const ticket_text = document.querySelector('textarea[name="ticket-text"]').value;
+const status = JSON.parse(document.getElementById('status').value);
+
+const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+        ];
+
+
 async function closeTicket() {
     
-    const title = document.querySelector('input[name="ticket-title"]').value.trim();
-    const ticket_text = document.querySelector('textarea[name="ticket-text"]').value;
+    
     let status = JSON.parse(document.getElementById('status').value);
-
-    const id = window.location.toString().split('/')[
-            window.location.toString().split('/').length - 1
-            ];
-
     status = !status 
 
     const response = await fetch(`/api/tickets/${id}`, {
@@ -44,3 +47,24 @@ async function closeTicket() {
     // }
     
 }
+
+const checkStatus = (status) => {
+
+
+    fetch(`/api/tickets/${id}`).then(function(res){
+        return res.json();
+    }).then(function(data){
+        console.log(data.status)
+    })
+
+    if(status === false) {
+        status = false;
+        document.getElementById('closeBtn').hidden = true;
+        document.getElementById('deleteBtn').hidden = true;
+    } else {
+        status = true;
+        document.getElementById('reopenBtn').hidden = false;
+    }
+}
+
+checkStatus();
