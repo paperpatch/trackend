a37f43ef7799fcdb3087b349243e35532d7dc7d8
Adam Calcasola <adam.calcasola@gmail.com>
2022-01-11 22:10:25 -0500

    fixed assigned select box options

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -88,84 +88,64 @@ router.get('/', withAuth, (req, res) => {
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
-  User.findAll({
-    // where: {
-    //   user_id: req.session.user_id
-    // },
+  Ticket.findByPk(req.params.id, {
     attributes: [
       'id',
-      'username'
+      'ticket_text',
+      'title',
+      'status',
+      'priority_id',
+      'status_change_id',
+      'created_at',
+      'due_date',
     ],
-    include: {
-      model: Ticket,
-      where: {
-        id: req.params.id,
-      },
-        attributes: [
-        'id',
-        'ticket_text',
-        'title',
-        'status',
-        'priority_id',
-        'status_change_id',
-        'type_id',
-        'assigned_id',
-        'created_at',
-        'due_date',
-      ],
-      include: [{
-          model: Comment,
-          attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username', 'role_id'],
-            include: {
-              model: Role,
-              attributes: ['role']
-            },
-          },
-        },
-        {
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
+        include: {
           model: User,
-          as: 'user',
-          attributes: ['username', 'role_id', ],
+          attributes: ['username', 'role_id'],
           include: {
             model: Role,
             attributes: ['role']
-          }
+          },
         },
-        {
-          model: User,
-          as: 'assign',
-          attributes: ['username'],
-        },
-        {
-          model: Priority,
-          attributes: ['level']
-        },
-        {
-          model: StatusChange,
-          attributes: ['statusChange']
-        },
-        {
-          model: Type,
-          attributes: ['type']
-        },
-      ]
-    }
+      },
+      {
+        model: User,
+        as: 'user',
+        attributes: ['username', 'role_id'],
+        include: {
+          model: Role,
+          attributes: ['role']
+        }
+      },
+      {
+        model: User,
+        as: 'assign',
+        attributes: ['username'],
+      },
+      {
+        model: Priority,
+        attributes: ['level']
+      },
+      {
+        model: StatusChange,
+        attributes: ['statusChange']
+      },
+      {
+        model: Type,
+        attributes: ['type']
+      },
+    ]
   })
     .then(dbTicketData => {
       if (dbTicketData) {
         console.log('===========================================');
-        const users = dbTicketData.map(user => user.get({plain:true}));
-        // const user = users.find(user => {user.id === req.session.user_id});
-        // const ticket = user.tickets.find(ticket => ticket.id === req.params.id);
-        // console.log(users);
-        // console.log(user);
-        // console.log(ticket);
+        const ticket = dbTicketData.get({plain:true});
         res.render('edit-ticket', {
-          users,
-          // ticket,
+          ticket,
           loggedIn: true,
           user_username: req.session.username
         });
