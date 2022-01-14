052e5ab77a8567bb0bbae561c671f55c3d8b834c
Adam Calcasola <adam.calcasola@gmail.com>
2022-01-13 19:16:42 -0500

    fix handlebars edit ticket

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -89,161 +89,91 @@ router.get('/', withAuth, (req, res) => {
 
 router.get('/edit/:id', withAuth, (req, res) => {
   User.findAll({
-    attributes: ['id', 'username']
-  })
-  .then(allUsers => {
-    const plainAllUsers = allUsers.map(user => user.get({
-      plain: true
-    }));
-    return plainAllUsers;
-  })
-  .then(users => {
-    Ticket.findByPk(req.params.id, {
-        attributes: [
-          'id',
-          'ticket_text',
-          'title',
-          'status',
-          'priority_id',
-          'status_change_id',
-          'type_id',
-          'assigned_id',
-          'created_at',
-          'due_date',
-        ],
-        include: [{
-            model: Comment,
-            attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
-            include: {
+      attributes: ['id', 'username']
+    })
+    .then(allUsers => {
+      const plainAllUsers = allUsers.map(user => user.get({
+        plain: true
+      }));
+      return plainAllUsers;
+    })
+    .then(users => {
+      Ticket.findByPk(req.params.id, {
+          attributes: [
+            'id',
+            'ticket_text',
+            'title',
+            'status',
+            'priority_id',
+            'status_change_id',
+            'type_id',
+            'assigned_id',
+            'created_at',
+            'due_date',
+          ],
+          include: [{
+              model: Comment,
+              attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username', 'role_id'],
+                include: {
+                  model: Role,
+                  attributes: ['role']
+                },
+              },
+            },
+            {
               model: User,
+              as: 'user',
               attributes: ['username', 'role_id'],
               include: {
                 model: Role,
                 attributes: ['role']
-              },
+              }
             },
-          },
-          {
-            model: User,
-            as: 'user',
-            attributes: ['username', 'role_id'],
-            include: {
-              model: Role,
-              attributes: ['role']
-            }
-          },
-          {
-            model: User,
-            as: 'assign',
-            attributes: ['username'],
-          },
-          {
-            model: Priority,
-            attributes: ['level']
-          },
-          {
-            model: StatusChange,
-            attributes: ['statusChange']
-          },
-          {
-            model: Type,
-            attributes: ['type']
-          },
-        ]
-      })
-      .then(dbTicketData => {
-        // const users = newUsers;
-        if (dbTicketData) {
-          // const users = dbTicketData[0].get({plain:true});
-          const ticket = dbTicketData.get({
-            plain: true
-          });
+            {
+              model: User,
+              as: 'assign',
+              attributes: ['username'],
+            },
+            {
+              model: Priority,
+              attributes: ['level']
+            },
+            {
+              model: StatusChange,
+              attributes: ['statusChange']
+            },
+            {
+              model: Type,
+              attributes: ['type']
+            },
+          ]
+        })
+        .then(dbTicketData => {
+          if (dbTicketData) {
+            const ticket = dbTicketData.get({
+              plain: true
+            });
 
-          res.render('edit-ticket', {
-            users,
-            ticket,
-            loggedIn: true,
-            user_username: req.session.username
-          });
-        } else {
-          res.status(404).end();
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  })
-  .catch(err => {
-    res.status(500).json(err);
-  });
-  // Ticket.findByPk(req.params.id, {
-  //   attributes: [
-  //     'id',
-  //     'ticket_text',
-  //     'title',
-  //     'status',
-  //     'priority_id',
-  //     'status_change_id',
-  //     'created_at',
-  //     'due_date',
-  //   ],
-  //   include: [
-  //     {
-  //       model: Comment,
-  //       attributes: ['id', 'comment_text', 'ticket_id', 'user_id', 'created_at'],
-  //       include: {
-  //         model: User,
-  //         attributes: ['username', 'role_id'],
-  //         include: {
-  //           model: Role,
-  //           attributes: ['role']
-  //         },
-  //       },
-  //     },
-  //     {
-  //       model: User,
-  //       as: 'user',
-  //       attributes: ['username', 'role_id'],
-  //       include: {
-  //         model: Role,
-  //         attributes: ['role']
-  //       }
-  //     },
-  //     {
-  //       model: User,
-  //       as: 'assign',
-  //       attributes: ['username'],
-  //     },
-  //     {
-  //       model: Priority,
-  //       attributes: ['level']
-  //     },
-  //     {
-  //       model: StatusChange,
-  //       attributes: ['statusChange']
-  //     },
-  //     {
-  //       model: Type,
-  //       attributes: ['type']
-  //     },
-  //   ]
-  // })
-  //   .then(dbTicketData => {
-  //     if (dbTicketData) {
-  //       console.log('===========================================');
-  //       const ticket = dbTicketData.get({plain:true});
-  //       res.render('edit-ticket', {
-  //         ticket,
-  //         loggedIn: true,
-  //         user_username: req.session.username
-  //       });
-  //     } else {
-  //       res.status(404).end();
-  //     }
-  //   })
-  //   .catch(err => {
-  //     res.status(500).json(err);
-  //   });  
+            res.render('edit-ticket', {
+              users,
+              ticket,
+              loggedIn: true,
+              user_username: req.session.username
+            });
+          } else {
+            res.status(404).end();
+          }
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
