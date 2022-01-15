fdc33ef7ec65c7eb504e62badbcc3983107f0f4d
Jake Toomey <jtoom238@gmail.com>
2022-01-15 14:34:21 -0500

    homepage layout/font

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+/*const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
@@ -9,5 +9,22 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
+
+module.exports = sequelize;
+
+*/
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 module.exports = sequelize;
