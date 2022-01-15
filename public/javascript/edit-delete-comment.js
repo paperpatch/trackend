ae1d006481b69e87651a2732dccfe98579d0621a
davezer <davezer27@gmail.com>
2022-01-15 13:09:46 -0500

    added edit and delete buttons to comments

---
+++ public/javascript/edit-delete-comment.js
@@ -0,0 +1,28 @@
+async function editComment(event) {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const ticket_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'PUT',
+        body: JSON.stringify({
+          ticket_id,
+          comment_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
