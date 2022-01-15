3f69a88777dd5cb45ec57cd9f7a38829f8ab8d1b
davezer <davezer27@gmail.com>
2022-01-15 14:40:41 -0500

    set up edit modal to look correctly

--- public/javascript/edit-delete-comment.js
+++ public/javascript/edit-delete-comment.js
@@ -1,13 +1,8 @@
 // let modal = document.getElementById("myModal");
 // let btn = document.getElementById("editButton");
-// let span = document.getElementsByClassName("close")[0];
 
 // btn.onclick = function() {
 //     modal.style.display = "block";
-// };
-
-// span.onclick = function() {
-//     modal.style.display = "none";
 // };
 
 // window.onclick = function(event) {
