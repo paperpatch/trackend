31c61a846a0007b4a3ce450b605950d4639a4d12
davezer <davezer27@gmail.com>
2022-01-15 13:49:48 -0500

    adding modals for editing/deleting comments

--- public/javascript/edit-delete-comment.js
+++ public/javascript/edit-delete-comment.js
@@ -1,3 +1,23 @@
+// let modal = document.getElementById("myModal");
+// let btn = document.getElementById("editButton");
+// let span = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// };
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// };
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+// };
+
+
+
 async function editComment(event) {
     event.preventDefault();
   
@@ -24,5 +44,7 @@ async function editComment(event) {
         alert(response.statusText);
       }
     }
-  }
+}
+
+
   
