77d21d6c4dc36ad3dffdd7b253405f9babe2ad91
paperpatch <patchen21@gmail.com>
2023-03-18 10:58:37 -0400

    revert connection. added dockerfile

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-});
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
